7560024e7c5fa75800c10cf1ab395cbe9e0602e5
Stepan Skichko <st.skichko@gmail.com>
2023-02-03 12:47:15 +0200

    task 1 DONE

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+// Add imports above this line
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+function createGallery() {
+  return galleryItems
+    .map(item => {
+      return `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`;
+    })
+    .join('');
+}
+
+galleryRef.innerHTML = createGallery(galleryItems);
+
+galleryRef.addEventListener('click', event => {
+  event.preventDefault();
+});
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
