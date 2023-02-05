f732ee57e9cbda2655e3a4067cf6f4abf8a86895
Stepan Skichko <st.skichko@gmail.com>
2023-02-05 15:44:59 +0200

    task 3 DONE
    task 2 left

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const emailRef = document.querySelector('input[name="email"]');
+const textRef = document.querySelector('textarea[name="message"]');
+
+formRef.addEventListener('input', throttle(onInputClick, 500)),
+  formRef.addEventListener('submit', onSubmitClick);
+
+formAutoFill();
+
+const formData = {};
+
+function onInputClick(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onSubmitClick(e) {
+  e.preventDefault();
+  const formDataJSON = localStorage.getItem('feedback-form-state');
+  const formDataParseJSON = JSON.parse(formDataJSON);
+  console.log(formDataParseJSON);
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function formAutoFill() {
+  const savedDataJSON = localStorage.getItem('feedback-form-state');
+
+  if (savedDataJSON) {
+    const savedDataParseJSON = JSON.parse(savedDataJSON);
+    emailRef.value = savedDataParseJSON.email;
+    textRef.value = savedDataParseJSON.message;
+  }
+}
