3302ff4ef266aeb8c00e59f55803dec5d627e673
Stepan Skichko <st.skichko@gmail.com>
2023-02-08 15:37:56 +0200

    little commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,8 +4,6 @@ import { galleryItems } from './gallery-items';
 // Add imports above this line
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryRef = document.querySelector('.gallery');
 
 function createGallery() {
